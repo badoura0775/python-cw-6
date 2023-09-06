492d9f1f4fe89e8125f51d5103c6c8c6b07a89b4
badoura485 <badreyaalmuzaini@gmail.com>
2023-09-06 19:45:26 +0300

    cw6

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # write your code here
+person = {
+    'name':'Badreya',
+    'age': 17,
+    "hobbies" : ['Drawing','Knitting','Reading','playing piano'],
+    }
+
+print(person.get('name'))
+print(person.get('age'))
+
+person['age']=1000
+person['country']= 'kuwait'
+print(person)
+print(len(person))
+person["hobbies"].append("watching anime")
+def check_hobbies(person):
+    if len(person["hobbies"])>3:
+        print('WOW YOU ARE AMAZING😎')
+check_hobbies(person)
